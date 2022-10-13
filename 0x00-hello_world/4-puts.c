6835a9bf3fdab24ad4d0c25208b5bb5b21c2a3a0
Nyanzi2022 <nyanzij@gmail.com>
2022-10-13 15:54:29 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,18 +4,12 @@
  *
  * * main - Entry point
  *
- * * Description: prints "Programming is like building a multilingual puzzle"
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+ * * Return: Always 0
+ */
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
+
